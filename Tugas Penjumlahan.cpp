360babb66af77d5fd2b4627c85b84dc784e70639
syarbianfirmansyah <syarbianfirmansyah@gmail.com>
2025-09-26 17:21:28 +0700

    Add files via upload

---
+++ Tugas Penjumlahan.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	
+	// penjumlahan
+	cout << "=== Oprasi Penjumlahan ===" << endl;
+	cout << " 5: ";
+	cin >> a;
+	cout << " 5: ";
+	cin >> b;
+	cout << 5 << " + " << 5 << " = " << 5 + 5 << endl << endl;
+	
+	// Pengurangan 
+	cout << "=== Oprasi pengurangan ===" << endl;
+	cout << " 6: ";
+	cin >> a;
+	cout << " 9: ";
+	cout << 6 << " - " << 9 << " = " << 6 - 9 << endl << endl;
+	
+	// Perkalian
+	cout << "=== Oprasi Perkalian ===" << endl;
+	cout << " 4: ";
+	cin >> a;
+	cout << " 7: ";
+	cin >> b;
+	cout << 4 << " * " << 7 << " = " << 4 * 7 << endl;
+	
+	return 0;
+	
+}
